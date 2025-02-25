2f0a4f5f8d788cb36b473e2f38e6dcddc5126084
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-02-25 15:03:15 +0000

    chore: moved main.c to src/

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-#include "../include/parse.h"
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
-int main(void)
+int main(int ac, char **av)
 {
+	char	*env_var;
+	char	*input;
+	pid_t pid;
+
+	(void)ac;
+	env_var = getenv("PATH");
+	printf("PATH: %s\n", env_var);
 	while (1)
-		read_input();
-	return (0);
+	{
+		input = readline("minishell>");
+		pid = fork();
+		if (pid == 0)
+		{
+			printf("fodasse\n");
+			execve(input, av + 1, NULL);
+		}
+		else
+		wait(NULL);
+	}
+    return 0;
 }
