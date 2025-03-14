e31fcab093bbc17d6bac5c0db73f04825537cd39
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-03-14 19:35:24 +0000

    chore(parser): syntax tree traversal examples

--- src/parser/syntax_tree.c
+++ src/parser/syntax_tree.c
@@ -25,7 +25,24 @@ static t_tree_node	*new_node(int val, char *str)
 	//new->str = ft_strdup(str);
 	return(new);
 }
+/*
+sample tree
 
+		  0
+	1			2
+3		4	5		6
+
+*/
+
+/*
+Sample : depth-first search (DFS) 0 1 3 4 2 5 6 -- stack data structure
+traverses from root and proceeds through the nodes as far as possible until we reach the node with no unvisited nodes nearby
+>> builds the tree sub-tree by sub-tree
+LIFO
+- inorder traversal : left-root-right
+- preorder traversal : root-left-right
+- postorder traversal : left-right-root
+*/
 static void print_depth_first(t_tree_node *root) //tmp function
 {
 	t_tree_node	*process_stack[100];
@@ -38,6 +55,13 @@ static void print_depth_first(t_tree_node *root) //tmp function
 		t_tree_node	*current = process_stack[process_stack_num - 1];
 		process_stack_num--;
 		printf("%d ", current->val);
+		if (current->val == 4)
+		{
+			printf("Found node %d (4)\n", current->val);
+			break ;
+		}
+		else
+			printf("Not yet: %d\n", current->val);
 		if (current->right != NULL)
 		{
 			process_stack[process_stack_num] = current->right;
@@ -50,3 +74,9 @@ static void print_depth_first(t_tree_node *root) //tmp function
 		}
 	}
 }
+
+/*
+Sample : breadth-first search (BFS) 0 1 2 3 4 5 6 -- queue data structure
+traverses tree level by level
+FIFO
+*/
