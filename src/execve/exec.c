56ab385ba395e79fa22605165a2cdcd0cd2a3841
Carlos Augusto Vaz Leal Teixeira <carlaugu@c1r4s3.42porto.com>
2025-03-18 14:27:55 +0000

    change: creat_env_arr function change

--- src/execve/exec.c
+++ src/execve/exec.c
@@ -6,7 +6,7 @@
 /*   By: carlaugu <carlaugu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:36:09 by carlaugu          #+#    #+#             */
-/*   Updated: 2025/03/18 12:24:02 by carlaugu         ###   ########.fr       */
+/*   Updated: 2025/03/18 14:24:49 by carlaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,48 +22,67 @@
 [ ] $? 127 if cmd not found
 */
 
-// int	find_slash(char *word)
-// {
-// 	while (*word)
-// 	{
-// 		if (*word == '/')
-// 			return (1);
-// 		word++;
-// 	}
-// 	return (0);
-// }
-// void	without_slash(t_data *data)
-// {
-// 	char	**env_path;
-// 	int	i;
+int	find_slash(char *word)
+{
+	while (*word)
+	{
+		if (*word == '/')
+			return (1);
+		word++;
+	}
+	return (0);
+}
+void	without_slash(t_data *data, t_word *word, char **tmp)
+{
+	char	**env_path;
+	int	i;
+	char	*tmp1;
 
-// 	env_path = set_path(data);
-// 	if (!env_path)
-// 	{
-// 		no_file_or_directory(data->word_lst.word->word);
-// 		return ;
-// 	}
-// 	i = -1;
-// 	while (env_path[++i])
-// 	{
-// 		tmp = ft_strjoin();
-		
-// 	}
-	
-// }
+	env_path = set_path(data);
+	if (!env_path)
+	{
+		no_file_or_directory(word->word);
+		return ;
+	}
+	i = -1;
+	while (env_path[++i])
+	{
+		tmp1 = ft_strjoin(env_path[i], word->word );
+		if (!tmp1)
+		{
+			error_allocation();
+			return ;
+		}
+		if (!access(tmp1, F_OK) && !access(tmp1, X_OK))
+		{
+			*tmp = tmp1;
+			return ;
+		}
+		free(tmp1);
+	}
+}
 
 int	exec(t_data *data, t_word *word)
 {
 	char	**wrd_arr;
 	char	**env_arr;
+	char	*tmp;
 
 	wrd_arr = creat_wrd_arr(word);
 	env_arr = creat_env_arr(data->env);
 	if (!wrd_arr || !env_arr)
 		return (free_arrays(wrd_arr, env_arr, 1));
-	// if (!find_slash(word->word))
-	// 	without_slash(data);
-	// else
-	// 	free_arrays(wrd_arr, env_arr, 0);
+	tmp = NULL;
+	if (!find_slash(word->word))
+		without_slash(data, word, &tmp);
+	if (tmp)
+	{
+		if (execve(tmp , wrd_arr, env_arr) < 0)
+		{
+			perror("execve failed");
+			exit(127); // code to return ???
+		}
+	}
+	free_arrays(wrd_arr, env_arr, 0);
 	return (0);
 }
