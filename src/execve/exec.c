334782ae187cb7771a72fd42e13072093663f323
Carlos688940 <carlosleal2000@gmail.com>
2025-03-18 22:43:42 +0000

    add: ERR_MEM 12 to errors.h
    add: opendir() to execve and is_valid_path()

--- src/execve/exec.c
+++ src/execve/exec.c
@@ -13,14 +13,6 @@
 #include "../../include/builtins.h"
 #include "../../include/execve.h"
 #include "../../include/errors.h"
-/*
-[ ]   /absolute/path/
-[ ]  ../../relative/path/
-[ ]  exec_name
-[ ] ./user_exec
-[ ] ./path/to/user_exec
-[ ] $? 127 if cmd not found
-*/
 
 int	find_slash(char *word)
 {
@@ -32,35 +24,76 @@ int	find_slash(char *word)
 	}
 	return (0);
 }
-
-void	without_slash(t_data *data, t_word *word, char **tmp)
+int	cmd_exist_in_path(t_data *data, t_word *word, char **tmp)
 {
 	char	**env_path;
-	int	i;
 	char	*tmp1;
+	int	i;
 
 	env_path = set_path(data);
 	if (!env_path)
-	{
-		no_file_or_directory(word->word);
-		return ;
-	}
+		return (no_file_or_directory(word->word));
 	i = -1;
 	while (env_path[++i])
 	{
 		tmp1 = ft_strjoin(env_path[i], word->word );
 		if (!tmp1)
-		{
-			error_allocation();
-			return ;
-		}
+			return (error_allocation());
 		if (!access(tmp1, F_OK) && !access(tmp1, X_OK))
 		{
 			*tmp = tmp1;
-			return ;
+			return (0);
 		}
 		free(tmp1);
 	}
+	return (command_not_found(word->word));
+}
+int	is_valid_path(t_word *word, char **tmp)
+{
+	DIR	*dir;
+	
+	dir = NULL;
+	if (!access(word->word, F_OK))
+	{
+		dir = opendir(word->word);
+		if (dir)
+		{
+			print_fd(2, "minishell: %s: Is a directory\n", word->word);
+			// update error variable after print message
+			// if (closedir(dir) < 0)
+				// printf("ola\n");
+		}
+		else if (!access(word->word, X_OK))
+			*tmp = word->word;
+		else
+		{
+			//TODO: see how to handle with this
+			print_fd(2, "minishell: %s: ", word->word);
+			perror(NULL);
+			// print_fd(2, "minishell: %s: %s", word->word, strerror(errno));
+		}
+	}
+	else
+		no_file_or_directory(word->word);
+	return (0);
+}
+
+void	execute(char *tmp, char **wrd_arr, char **env_arr)
+{
+	pid_t	pid;
+
+	pid = fork();
+	// if (pid < 0)
+	if (pid == 0)
+	{
+		if (execve(tmp , wrd_arr, env_arr) < 0)
+		{
+			perror("minishell");
+			//exit ------>> with our exit
+		}
+	}
+	else
+		wait(NULL);
 }
 
 int	exec(t_data *data, t_word *word)
@@ -75,14 +108,13 @@ int	exec(t_data *data, t_word *word)
 		return (free_arrays(wrd_arr, env_arr, 1));
 	tmp = NULL;
 	if (!find_slash(word->word))
-		without_slash(data, word, &tmp);
+		cmd_exist_in_path(data, word, &tmp); // this return an error code
+	else
+		is_valid_path(word, &tmp);
 	if (tmp)
 	{
-		if (execve(tmp , wrd_arr, env_arr) < 0)
-		{
-			perror("execve failed");
-			exit(127); // code to return ???
-		}
+		execute(tmp, wrd_arr, env_arr);
+		free(tmp);
 	}
 	free_arrays(wrd_arr, env_arr, 0);
 	return (0);
