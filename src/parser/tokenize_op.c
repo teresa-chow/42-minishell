3d75d02bc4181bb1b1ee87e3df3c9a57f82dad44
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-03-24 14:47:54 +0000

    fix(parser): operator sequence tokenization (not accepting >| now)

--- src/parser/tokenize_op.c
+++ src/parser/tokenize_op.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 15:13:04 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/03/24 10:37:57 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/03/24 14:45:29 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,8 @@ static unsigned int	substr_count(char const *input)
 	count = 0;
 	while (input[i])
 	{
-		if (input[i] && !is_operator(input[i]))
+		if ((input[i] && !is_operator(input[i]))
+			|| (input[i] == '&' && !is_equal_next(input, i)))
 		{
 			count++;
 			i = substr_count_no_op(input, i);
@@ -76,20 +77,12 @@ static unsigned int	substr_count_no_op(const char *input, unsigned int i)
 {
 	while (input[i])
 	{
-		if (is_redirection(input[i]))
-		{
-			while (is_redirection(input[i]))
-				i++;
-			while (input[i] == '|')
-				i++;
-		}
+		while (is_redirection(input[i]))
+			i++;
+		if (input[i] == '&' && !is_equal_next(input, i))
+			i++;
 		if (is_operator(input[i]))
-		{
-			if (input[i] == '&' && !is_operator(input[i + 1]))
-				i++;
-			else
-				break ;
-		}
+			break ;
 		if ((int)input[i] == '(')
 			i += group_len(input, i);
 		else if (is_quote((int)input[i]))
@@ -105,11 +98,12 @@ static unsigned int	substr_len(const char *str)
 	unsigned int	i;
 
 	i = 0;
-	if (!is_operator(str[i]))
+	if ((!is_operator(str[i]))
+		|| (str[i] == '&' && !is_equal_next(str, i)))
 		i = substr_len_no_op(str, i);
-	else if (is_operator(str[i]))
+	else
 	{
-		while (str[i] && is_equal_next(str, i))
+		while (str[i] && is_equal_next(str, i)) // single & scenario
 			i++;
 		i++;
 		while (is_delimiter(str[i]))
@@ -122,20 +116,12 @@ static unsigned int	substr_len_no_op(const char *str, unsigned int i)
 {
 	while (str[i])
 	{
-		if (is_redirection(str[i]))
-		{
-			while (is_redirection(str[i]))
-				i++;
-			while (str[i] == '|')
-				i++;
-		}
+		while (is_redirection(str[i]))
+			i++;
+		if (str[i] == '&' && !is_equal_next(str, i))
+			i++;
 		if (is_operator(str[i]))
-		{
-			if (str[i] == '&' && !is_operator(str[i + 1]))
-				i++;
-			else
-				break ;
-		}
+			break ;
 		if ((int)str[i] == '(')
 			i += group_len(str, i);
 		else if (is_quote((int)str[i]))
