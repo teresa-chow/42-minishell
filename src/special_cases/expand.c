e6536f0eef4e8579ea6864c85fece67a63bccc67
Carlos688940 <carlosleal2000@gmail.com>
2025-03-30 22:45:51 +0000

    update

--- src/special_cases/expand.c
+++ src/special_cases/expand.c
@@ -104,21 +104,16 @@ void	expand(t_data *data, t_word *word)
 
 	param = ft_strchr(word->word, '$') + 1;
 	last_part = ft_strchr(param, '"');
-	last_part = ft_strchr(param, find_no_alnum(param));
+	last_part = ft_strchr(param, find_no_alnum(param)); //// fix: i cant have '"'. if found '.!"', i dont want "
 	if (*last_part == '"')
 		last_part = NULL;
 	param = ft_substr(param, 0, ft_strlen(param) - ft_strlen(ft_strchr(param, find_no_alnum(param))));
 	// if (!param)
 	env_var = get_var(data->env, param);
-	if (last_part)
-	{
-		new = ft_calloc(sizeof(t_word), sizeof(char));
-		new->word = ft_strdup(last_part);
-		word->next = new;
-		new->next = data->exp_data->next_arg;
-	}
 	if (env_var)
-	{
+	{												//$OLDPWD.!					
+		////// if last part is valid, i have to join last part to expansion   ----------- like: /home/carlaugu.!
+
 		free(word->word);
 		word->word = ft_strdup(env_var->val);
 	}
