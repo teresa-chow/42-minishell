b87b428e9c606bb5527ae4383da215289a352315
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-04-09 16:41:48 +0100

    chore: working on ast group handling (wip)

--- src/parser/syntax_tree.c
+++ src/parser/syntax_tree.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 18:20:27 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/04/08 11:54:01 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/09 16:40:53 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 #include "../../include/utils.h"
 
 static t_word_lst	*find_pivot(t_word_lst *start, t_word_lst *end);
-//static int	is_group(t_word_lst *word_lst);
-//static void	handle_cmd_group(t_word_lst *pivot, int index, t_tree_node **node);
+static void	handle_cmd_group(t_word_lst *pivot, int index, t_tree_node **node);
 //static void	rm_parentheses(t_word_lst *word_lst, t_tree_node **node);
 
 void	create_syntax_tree(t_word_lst *start, t_word_lst *end, int index,
@@ -28,11 +27,8 @@ void	create_syntax_tree(t_word_lst *start, t_word_lst *end, int index,
 	new_start = NULL;
 	new_end = NULL;
 	pivot = find_pivot(start, end);
-	/*if (is_group(pivot)) //tmp solution, needs refactoring
-	{
+	if (pivot->word->word[0] == '(') //tmp solution, needs refactoring
 		handle_cmd_group(pivot, index, node);
-		return ;
-	}*/
 	fill_node(pivot, index, node);
 	if (index)
 		++index;
@@ -66,24 +62,16 @@ static t_word_lst	*find_pivot(t_word_lst *start, t_word_lst *end)
 	return (pivot);
 }
 
-/*static int	is_group(t_word_lst *word_lst)
-{
-	if (!word_lst->next && word_lst->word->word[0] == '(')
-		return (1);
-	return (0);
-}*/
-
 // TODO: handle groups, currently only handling single command group as input
-/*static void	handle_cmd_group(t_word_lst *pivot, int index, t_tree_node **node)
+static void	handle_cmd_group(t_word_lst *pivot, int index, t_tree_node **node)
 {
-	if (!index && !pivot->next)
-	{
-		rm_parentheses(pivot, node);
-		return ;
-	}
+	(*node)->word = "()";
+	(*node)->index = index;
+	(*node)->left = add_node();
+//	create_syntax_tree(start, new_end, ++index, &(*node)->left);
 }
 
-static void	rm_parentheses(t_word_lst *word_lst, t_tree_node **node)
+/*static void	rm_parentheses(t_word_lst *word_lst, t_tree_node **node)
 {
 	char		*tmp_group;
 	char		**cmd_lst;
