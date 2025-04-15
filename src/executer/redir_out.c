1d5e5ce1d1310c9d063ad2521bb207d5e751e10b
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-04-15 13:50:28 +0100

    refactor: added O_TRUNC flag to open

--- src/executer/redir_out.c
+++ src/executer/redir_out.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/11 12:11:15 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/04/14 14:28:58 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/15 13:34:22 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,9 +106,9 @@ static void	create_files(int *fd, char **files, int count, int type)
 	while (i < count)
 	{
 		if (type == 1)
-			fd[i] = open(files[i], O_WRONLY | O_CREAT, 0644);
+			fd[i] = open(files[i], O_WRONLY | O_TRUNC | O_CREAT, 0644);
 		else if (type == 2)
-			fd[i] = open(files[i], O_WRONLY | O_CREAT | O_APPEND, 0644);
+			fd[i] = open(files[i], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0644);
 		if (fd[i] == -1)
 		{
 			print_fd(STDERR_FILENO, "minishell: %s: ", files[i]);
