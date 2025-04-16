3da0d7228cc4ec9c126a9b766305a0325b8eafc3
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-04-16 11:48:53 +0100

    chore: add heredoc comments

--- src/executer/heredoc.c
+++ src/executer/heredoc.c
@@ -6,7 +6,13 @@
 /*   By: tchow-so <tchow-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/16 10:59:18 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/04/16 10:59:21 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/16 11:45:02 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../../include/parse.h"
+#include "../../include/execute.h"
+#include "../../include/utils.h"
+
+//end of file will not expand (analyze heredoc prior to expansions), content will
+//readline
