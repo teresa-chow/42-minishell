25e9d3ed6eea6287b840fd2deadc1c3f5fd29b5c
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-04-21 09:52:48 +0100

    chore: add handle wildcard to execution

--- src/executer/exec_ast.c
+++ src/executer/exec_ast.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:00:09 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/04/17 15:05:24 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/21 09:52:06 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,6 +64,8 @@ int	exec_ast_cmd(t_data *data, t_tree_node **node, int *i)
 	save_old_in_out(&old_stdin, &old_stdout);
 	//redir_heredoc(data, (*node)->word);
 	if (handle_tokens((*node)->word, data, node) == -1)
+		return (-1);
+	if (handle_wildcard((*node)->word, data) == -1)
 		return (-1);
 	if (redir_in_out_check((*node)->word, data) != 0)
 		return (-1);
