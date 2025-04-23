ab17c7515b1b7a21be96763f5411778869735193
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-04-23 11:35:06 +0100

    chore(pipe): not redirecting STDOUT to next cmd STDIN successfully

--- src/executer/exec_ast_pipe.c
+++ src/executer/exec_ast_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 11:24:52 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/04/22 22:35:49 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/23 11:32:40 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@
 #include "../../include/errors.h"
 
 static int	create_pipe(int	fd[2]);
-static void	pipe_output(t_data *data, t_tree_node **node_right, int *i,
-	int fd[2]);
 static void	pipe_input(t_data *data, t_tree_node **node_right, int *i,
+	int fd[2]);
+static void	pipe_output(t_data *data, t_tree_node **node_right, int *i,
 	int fd[2]);
 
 /* Obs.: fd[0] is set up for reading, fd[1] is set up for writing */
@@ -27,10 +27,10 @@ void	ast_handle_pipe(t_data *data, t_tree_node **node, int *i)
 {
 	int	fd[2];
 
-	if (create_pipe(&fd[2]) == -1)
+	if (create_pipe(fd) == -1)
 		return ;
-	pipe_output(data, &(*node)->right, i, &fd[2]);
-	pipe_input(data, &(*node)->left, i, &fd[2]);
+	pipe_input(data, &(*node)->left, i, fd);
+	pipe_output(data, &(*node)->right, i, fd);
 	close(fd[1]);
 	close(fd[0]);
 }
@@ -45,35 +45,6 @@ static int	create_pipe(int	fd[2])
 	return (0);
 }
 
-static void	pipe_output(t_data *data, t_tree_node **node_right, int *i, int fd[2])
-{
-	int	id;
-
-	if (is_builtin_cmd(node_right))
-	{
-		id = fork();
-		if (id == -1)
-		{
-			ft_putstr_fd("Minishell: fork error\n", STDERR_FILENO);
-			return ;
-		}
-		if (id == 0)
-		{
-			close(fd[1]);
-			dup2(fd[0], STDIN_FILENO);
-			exec_ast_cmd(data, node_right, i);
-			close(fd[0]);
-		}
-	}
-	else
-	{
-		close(fd[1]);
-		dup2(fd[0], STDIN_FILENO);
-		ast_depth_search(data, node_right, i);
-		close(fd[0]);
-	}
-}
-
 static void	pipe_input(t_data *data, t_tree_node **node_left, int *i, int fd[2])
 {
 	int	id;
@@ -82,10 +53,7 @@ static void	pipe_input(t_data *data, t_tree_node **node_left, int *i, int fd[2])
 	{
 		id = fork();
 		if (id == -1)
-		{
-			ft_putstr_fd("Minishell: fork error\n", STDERR_FILENO);
-			return ;
-		}
+			return (ft_putstr_fd("Minishell: fork error\n", STDERR_FILENO));
 		if (id == 0)
 		{
 			close(fd[0]);
@@ -93,6 +61,8 @@ static void	pipe_input(t_data *data, t_tree_node **node_left, int *i, int fd[2])
 			exec_ast_cmd(data, node_left, i);
 			close(fd[1]);
 		}
+		else
+			waitpid(id, NULL, 0);
 	}
 	else
 	{
@@ -100,5 +70,33 @@ static void	pipe_input(t_data *data, t_tree_node **node_left, int *i, int fd[2])
 		dup2(fd[1], STDIN_FILENO);
 		ast_depth_search(data, node_left, i);
 		close(fd[1]);
+	}
+}
+
+static void	pipe_output(t_data *data, t_tree_node **node_right, int *i, int fd[2])
+{
+	int	id;
+
+	if (is_builtin_cmd(node_right))
+	{
+		id = fork();
+		if (id == -1)
+			return (ft_putstr_fd("Minishell: fork error\n", STDERR_FILENO));
+		if (id == 0)
+		{
+			close(fd[1]);
+			dup2(fd[0], STDOUT_FILENO);
+			exec_ast_cmd(data, node_right, i);
+			close(fd[0]);
+		}
+		else
+			waitpid(id, NULL, 0);
+	}
+	else
+	{
+		close(fd[1]);
+		dup2(fd[0], STDOUT_FILENO);
+		ast_depth_search(data, node_right, i);
+		close(fd[0]);
 	}
 }
