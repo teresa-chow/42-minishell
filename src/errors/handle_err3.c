6da524f660f928062969663d1feea004500ad26d
Teresa Chow <teresa-chow@users.noreply.github.com>
2025-04-30 23:31:03 +0100

    chore: standard compliance

--- src/errors/handle_err3.c
+++ src/errors/handle_err3.c
@@ -6,15 +6,15 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:02:24 by carlaugu          #+#    #+#             */
-/*   Updated: 2025/04/30 17:00:02 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/04/30 23:30:42 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/expand.h"
 
-int    error_open_dir(t_data *data)
+int	error_open_dir(t_data *data)
 {
-    perror ("Cannot open current directory");
+	perror ("Cannot open current directory");
 	data->exit_status = ERR;
 	return (-1);
 }
@@ -22,5 +22,5 @@ int    error_open_dir(t_data *data)
 void	heredoc_error(char *token)
 {
 	print_fd(STDERR_FILENO, "minishell: warning: here-document "
-	"delimited by end-of-file (wanted `%s')\n", token);
+		"delimited by end-of-file (wanted `%s')\n", token);
 }
