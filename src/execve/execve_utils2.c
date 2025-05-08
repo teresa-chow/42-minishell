19c9ff26f6b18d63cbb43eb5a1f42fbf0220d0e0
Carlos Augusto Vaz Leal Teixeira <carlaugu@c1r4s4.42porto.com>
2025-05-08 17:08:15 +0100

    add close_heredoc_fds in free_failed_child

--- src/execve/execve_utils2.c
+++ src/execve/execve_utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execve_utils2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: carlaugu <carlaugu@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:36:50 by tchow-so          #+#    #+#             */
-/*   Updated: 2025/05/08 16:53:20 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/05/08 17:06:17 by carlaugu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	free_failed_child(t_exec_data *inf, t_data *data, t_tree_node *node)
 {
 	free_arrays(inf, data, 0);
 	free_env_list(data, 0, &data->env);
+	close_heredoc_fds(data, NULL);
 	if (data->pipeline != NULL)
 		free_pipeline(data->pipeline);
 	reset_old_in_out(data, node);
