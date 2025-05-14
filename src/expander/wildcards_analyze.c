52a57514fa7c8ee32edb0e21a8d112c464fc0c32
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2025-05-14 14:58:54 +0100

    fix: wilcard expansion regarding directories (base case to do */ )

--- src/expander/wildcards_analyze.c
+++ src/expander/wildcards_analyze.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <tchow-so@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 17:34:28 by carlaugu          #+#    #+#             */
-/*   Updated: 2025/05/14 14:50:31 by tchow-so         ###   ########.fr       */
+/*   Updated: 2025/05/14 14:58:43 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,34 +45,6 @@ int	handle_wildcard(t_word *word, t_data *data)
 		word = word->next;
 	}
 	free_wild(data, 0, NULL);
-	return (0);
-}
-
-int	check_directory(struct dirent *entry, char *s, bool *check, t_data *data)
-{
-	struct stat	i_stat;
-	char		*str;
-
-	if (entry->d_name[0] != '.' && s[ft_strlen(s) - 1] == '/')
-	{
-		ft_bzero(&i_stat, sizeof(i_stat));
-		if (stat(entry->d_name, &i_stat) == -1)
-		{
-			perror("minishell: stat");
-			data->exit_status = ERR;
-			return (-1);
-		}
-		str = ft_substr(s, 0, ft_strlen(s) - 1);
-		if (is_matching_pattern(str, entry->d_name, data))
-		{
-			if (S_ISDIR(i_stat.st_mode))
-			{
-				*check = true;
-				data->wild->print_dir = true;
-			}
-		}
-		free(str);
-	}
 	return (0);
 }
 
